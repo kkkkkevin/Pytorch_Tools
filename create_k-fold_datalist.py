1ade15446c2d28f15e23c54888452562af8b5260
kkkkkevin <3143039+kkkkkevin@users.noreply.github.com>
2021-01-12 08:47:45 +0000

    add kfold creater label

--- create_k-fold_datalist.py
+++ create_k-fold_datalist.py
@@ -2,16 +2,29 @@ import os
 from sklearn.model_selection import KFold
 
 
+def get_fold_indices(imgs, anns):
+    kf = KFold(5, shuffle=True)
+    fold_list = []
+    for train_index, test_index in kf.split(imgs):
+        fold_list.append({
+            'train_index': train_index,
+            'test_index': test_index
+        })
+
+    return fold_list
+
+
 def main(img_dir, ann_dir, save_path):
 
     imgs = list(sorted(os.listdir(img_dir)))
     anns = list(sorted(os.listdir(ann_dir)))
 
-    kf = KFold(5, shuffle=True)
+    fold_list = get_fold_indices(imgs, anns)
+
     fold_no = 0
-    for train_index, test_index in kf.split(imgs):
-        train_fl = [img_dir + imgs[i] for i in train_index]
-        test_fl = [img_dir + imgs[i] for i in test_index]
+    for fold in fold_list:
+        train_fl = [img_dir + imgs[i] for i in fold['train_index']]
+        test_fl = [img_dir + imgs[i] for i in fold['test_index']]
         with open(save_path + '/' + 'train' + f'{fold_no}' + '.txt', mode='w') as tx:
             tx.write('\n'.join(train_fl))
         with open(save_path + '/' + 'val' + f'{fold_no}' + '.txt', mode='w') as tx:
